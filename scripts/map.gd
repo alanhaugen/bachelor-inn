fdc76470c00e79ed0702ef0793b2b3c4c6fde01d
Hans Alan Whitburn Haugen <alanhaugen@gmail.com>
2025-10-14 15:52:01 +0200

    Selecting tiles now working

--- scripts/map.gd
+++ scripts/map.gd
@@ -1,8 +1,27 @@
 extends Node2D
 
-func _init() -> void:
-	
-	return
+var tiles :Array;
+var units :Array;
 
-func _process(delta: float) -> void:
-	return
+@onready var cursor: Sprite2D = $Cursor;
+@onready var map :TileMapLayer = %Map;
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		var pos :Vector2 = map.local_to_map(event.position / map.transform.get_scale());
+		cursor.position = map.map_to_local(pos * 5);
+		map.set_cell(pos,1);
+		cursor.show();
+	#elif event is InputEventMouseMotion:
+	#	print("Mouse Motion at: ", event.position)
+
+
+#func _init() -> void:
+
+func _ready() -> void:
+	cursor.hide();
+	#tiles = map.get_used_cells();
+#	units.append(unit);
+
+#func _process(delta: float) -> void:
+#	return
