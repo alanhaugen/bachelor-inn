58de753f29368285eed16373262f4e1b4fa386b6
Hans Alan Whitburn Haugen <alanhaugen@gmail.com>
2025-11-03 09:37:09 +0100

    Save format is saved

--- scripts/save_game.gd
+++ scripts/save_game.gd
@@ -23,7 +23,7 @@ func create_new_save_data() -> void:
 				{
 					"hp": 15, 
 					"max_hp": 15,
-					"strenght": 5, 
+					"strength": 5, 
 					"magic": 10,
 					"skill": 10, 
 					"speed": 5,
@@ -115,6 +115,7 @@ func create_new_save_data() -> void:
 	}
 	
 	var saves := {
+		"Noble Nights Save format": version,
 		"Slot 1":
 		{
 			"level": "first",
@@ -169,29 +170,30 @@ func read(save_slot: int) -> bool:
 		return false; # Error! We don't have a save to load.
 
 	var save_file: Object = FileAccess.open(SAVE_GAME_PATH, FileAccess.READ);
-	while save_file.get_position() < save_file.get_length():
-		var json_string: String = save_file.get_line();
+	var json_string: String = save_file.get_as_text();
 
-		# Creates the helper class to interact with JSON.
-		var json: JSON = JSON.new()
+	# Creates the helper class to interact with JSON.
+	var json: JSON = JSON.new()
 
-		# Check if there is any error while parsing the JSON string, skip in case of failure.
-		var parse_result: Error = json.parse(json_string)
-		if not parse_result == OK:
-			print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
-			continue
+	# Check if there is any error while parsing the JSON string, skip in case of failure.
+	var parse_result: Error = json.parse(json_string)
+	if not parse_result == OK:
+		print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
+		return false;
 
-		# Get the data from the JSON object.
-		var node_data: Variant = json.data;
+	# Get the data from the JSON object.
+	var save: Dictionary = json.data;
+	
+	print(save);
 
-		# Firstly, we need to create the object and add it to the tree and set its position.
-		#var new_object = load(node_data["filename"]).instantiate()
-		#get_node(node_data["parent"]).add_child(new_object)
-		#new_object.position = Vector2(node_data["pos_x"], node_data["pos_y"])
+	# Firstly, we need to create the object and add it to the tree and set its position.
+	#var new_object = load(node_data["filename"]).instantiate()
+	#get_node(node_data["parent"]).add_child(new_object)
+	#new_object.position = Vector2(node_data["pos_x"], node_data["pos_y"])
 
-		# Now we set the remaining variables.
-		#for i in node_data.keys():
-		#	if i == "filename" or i == "parent" or i == "pos_x" or i == "pos_y":
-		#		continue
-		#	new_object.set(i, node_data[i])
+	# Now we set the remaining variables.
+	#for i in node_data.keys():
+	#	if i == "filename" or i == "parent" or i == "pos_x" or i == "pos_y":
+	#		continue
+	#	new_object.set(i, node_data[i])
 	return true;
