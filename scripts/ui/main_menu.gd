cfbeb640c2e98c83438bc680e2b18720e49914a8
Hans Alan Whitburn Haugen <alanhaugen@gmail.com>
2025-11-05 13:59:20 +0100

    UIs added

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -7,13 +7,15 @@ extends Node3D
 @onready var gui: Control = $UI;
 
 ## Level buttons
-@onready var levelButton: Button = $UI/MapSelector/LoadMap0;
+@onready var levelButton: Button = $UI/VBoxContainer/LevelSelect/LoadMap0;
 
 #region: --- Processing ---
 ## Called when the node enters the scene tree for the first time
 func _ready() -> void:
 	Main.gui = gui;
 	Main.world = world;
+	
+	$UI/VBoxContainer/LevelSelect.visible = false;
 	
 	print(OS.get_data_dir());
 	
@@ -32,12 +34,26 @@ func _ready() -> void:
 #region: --- Signals ---
 ## Called when the Load Map 0 button is pressed
 func _on_load_map_0_pressed() -> void:
-	$UI/MapSelector.visible = false;
+	$UI/VBoxContainer/LevelSelect.visible = false;
 	Main.save.read(0);
 func _on_load_map_1_pressed() -> void:
-	$UI/MapSelector.visible = false;
+	$UI/VBoxContainer/LevelSelect.visible = false;
 	Main.save.read(1);
 func _on_load_map_2_pressed() -> void:
-	$UI/MapSelector.visible = false;
+	$UI/VBoxContainer/LevelSelect.visible = false;
 	Main.save.read(2);
 #endregion
+
+
+func _on_start_game_button_pressed() -> void:
+	$UI/VBoxContainer/LevelSelect.visible = true;
+	$UI/VBoxContainer/MainMenu.visible = false;
+
+
+func _on_back_button_pressed() -> void:
+	$UI/VBoxContainer/LevelSelect.visible = false;
+	$UI/VBoxContainer/MainMenu.visible = true;
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit();
