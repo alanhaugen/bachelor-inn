3848d4c115d724706d7abf945b2ceef48e7ba984
Hans Alan Whitburn Haugen <alanhaugen@gmail.com>
2026-01-08 19:46:04 +0100

    Started thinking about events to trigger from dialogic

--- scripts/combat/event.gd
+++ scripts/combat/event.gd
@@ -11,3 +11,17 @@ static func set_objective_defeat_all() -> void:
 
 static func set_objective_find_object(obect_name : String) -> void:
 	Main.battle_log.text = "Objective: Find orb";
+
+
+static func move_camera(pos : Vector3) -> void:
+	pass
+	#CameraManager.focus(pos)
+
+
+static func spawn_enemy(type : String, pos : Vector3i) -> void:
+	pass
+	#UnitSpawner.spawn(type, pos)
+
+
+#static func move_unit(unit_id : int, pos : Vector3i):
+#	GameController.force_move(unit_id, pos)
