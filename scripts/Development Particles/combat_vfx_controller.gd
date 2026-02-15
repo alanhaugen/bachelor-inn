568919d45ce895f466e7593c08eafc991f389fa2
Arctie <91278128+Arctie@users.noreply.github.com>
2026-02-15 22:44:59 +0100

    Melee attack exists

--- scripts/Development Particles/combat_vfx_controller.gd
+++ scripts/Development Particles/combat_vfx_controller.gd
@@ -4,6 +4,7 @@ class_name CombatVFXController
 @export var damage_number_scene : PackedScene
 @export var hit_particles_scene : PackedScene
 @export var ranged_attack_scene : PackedScene
+@export var melee_attack_scene  : PackedScene
 
 func play_attack(result : AttackResult) -> void:
 	if result == null:
@@ -12,10 +13,7 @@ func play_attack(result : AttackResult) -> void:
 	var attacker : Character= result.aggressor
 	if attacker and attacker.state and attacker.state.weapon:
 		if attacker.state.weapon.is_melee():
-			_spawn_hit_particles(result.victim)
-			_spawn_dmg_number_scene(result)
-			_trigger_hit_flash(result.victim, result.was_critical)
-			await get_tree().create_timer(0.1).timeout
+			await _spawn_melee_attack(attacker, result.victim, result)
 		else:
 			await _spawn_ranged_attack(attacker, result.victim, result)
 
@@ -54,6 +52,35 @@ func _trigger_hit_flash(target : Character, crit : bool) -> void:
 		target.flash_hit(crit)
 
 
+func _spawn_melee_attack(attacker : Character, target : Character, result: AttackResult) -> void:
+	if not melee_attack_scene:
+		return
+	var slice := melee_attack_scene.instantiate()
+	get_tree().current_scene.add_child(slice)
+	
+	var start_pos: = attacker.global_position + Vector3(0, 0, 0)
+	var end_pos: = target.global_position + Vector3(0, 0, 0)
+	slice.global_position = start_pos
+	slice.look_at(end_pos, Vector3.UP)
+	slice.rotation_degrees.y -= 60.0
+	
+	var tween: = slice.create_tween()
+	tween.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	
+	tween.tween_property(
+		slice, 
+		"rotation_degrees:y", 
+		slice.rotation_degrees.y + 30.0, 
+		0.2
+	)
+	
+	
+	await tween.finished
+	slice.queue_free()
+	_spawn_hit_particles(target)
+	_spawn_dmg_number_scene(result)
+	_trigger_hit_flash(target, result.was_critical)
+	
 func _spawn_ranged_attack(attacker : Character, target : Character, result: AttackResult) -> void:
 	if not ranged_attack_scene:
 		return
