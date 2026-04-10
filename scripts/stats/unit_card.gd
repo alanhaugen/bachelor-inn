1019cb5eb417fb18ae417a8d24fac236607dd9b5
Andreas A. Brandsnes <giveA@gmail.com>
2026-04-10 10:52:29 +0200

    UnitCard control node work

--- scripts/stats/unit_card.gd
+++ scripts/stats/unit_card.gd
@@ -6,11 +6,20 @@ class_name UnitCard
 @onready var health: Label = %Health
 @onready var sanity: Label = %Sanity
 @onready var weapon: Label = %Weapon
-@onready var strenght: Label = %Strenght
+@onready var strength: Label = %Strenght
 @onready var mind : Label = $Mind
 @onready var speed: Label = %Speed
 @onready var endurance: Label = %Endurance
 @onready var focus: Label = %focus
 
 func setup(character: Character) -> void:
-	pass
+	portrait.texture = character.portrait
+	unit_name.text = character.data.unit_name
+	health.text = "HP: %d/%d" % [character.state.current_health, character.state.max_health]
+	sanity.text = "SAN: %d/%d" % [character.state.current_sanity, character.state.max_sanity]
+	weapon.text = "WEP: %s" % character.state.weapon.weapon_name if character.state.weapon else "None"
+	strength.text = "STR: %d" % character.data.strength
+	mind.text = "MND: %d" % character.data.mind
+	speed.text = "SPD: %d" % character.data.speed
+	endurance.text = "END: %d" % character.data.endurance
+	focus.text = "FOC: %d" % character.data.focus
