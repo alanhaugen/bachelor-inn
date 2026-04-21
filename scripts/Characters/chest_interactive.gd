f1bfbbe1c095ed3cb0e44b23f91e1b5874d93cf7
AndreasBrandsnes <143383180+AndreasBrandsnes@users.noreply.github.com>
2026-04-21 12:25:50 +0200

    Added different loots to chests

--- scripts/Characters/chest_interactive.gd
+++ scripts/Characters/chest_interactive.gd
@@ -1,7 +1,11 @@
 extends Node3D
 class_name Chest
 
+enum LootType {WEAPON, SKILL}
+
+@export var loot_type : LootType = LootType.WEAPON
 @export var weapon_id: String = ""
+@export var skill_id: String = ""
 @export var dialogue_timeline: String = "chest_tutorial"
 var is_opened: bool = false
 var is_looted: bool = false
