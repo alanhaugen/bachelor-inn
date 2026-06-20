cfdd274952b6700992f3be1006630165d3ac7519
AndreasBrandsnes <143383180+AndreasBrandsnes@users.noreply.github.com>
2026-06-20 15:24:42 +0200

    Update enem_health_bar_overhead

--- scripts/ui/health_bar_enemy_overhead.gd
+++ scripts/ui/health_bar_enemy_overhead.gd
@@ -8,4 +8,39 @@ var character: Character
 var _trail_tween: Tween
 
 func _ready() -> void:
-	pass
+	character = get_parent() as Character
+	if character == null:
+		push_warning("HealthBarEnemy found no parent.")
+		return
+	
+	Main.level.character_stats_changed.connect(_on_character_stats_changed)
+	
+	var percent := float(character.state.current_health) / character.state.max_health
+	current.scale.x = percent
+	trail.scale.x = percent
+	visible = percent < 1.0
+
+func _on_character_stats_changed(changed_character: Character) -> void:
+	if changed_character != character:
+		return
+	
+	var percent := float(character.state.current_health) / character.state.max_health
+	set_health_percent(percent)
+
+func set_health_percent(new_percent: float) -> void:
+	new_percent = clamp(new_percent, 0.0, 1.0)
+	trail.scale.x = current.scale.x
+	current.scale.x = new_percent
+	
+	if _trail_tween:
+		_trail_tween.kill()
+	_trail_tween = create_tween()
+	_trail_tween.tween_property(trail, "scale.x", new_percent, 0.4).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
+	
+	visible = new_percent < 1.0
+	
+	if new_percent <= 0.0:
+		await _trail_tween.finished
+		queue_free()
+	
+	
