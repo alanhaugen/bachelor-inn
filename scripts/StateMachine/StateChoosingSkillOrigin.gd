40c44dfe551fa6c7a7db03f36c4bf49d988af811
Andreas <anbrandsnes@gmail.com>
2026-06-22 19:54:59 +0200

    Added new state ChoosingSkillOrigin

---
+++ scripts/StateMachine/StateChoosingSkillOrigin.gd
@@ -0,0 +1,13 @@
+extends LevelState
+class_name StateChoosingSkillOrigin
+
+## This is where we decide where to cast skills from
+
+func _enter(level: Node) -> void:
+	pass
+	
+func _exit(level: Node) -> void:
+	pass
+
+func handle_input(level: Node, event: InputEvent) -> void:
+	pass
