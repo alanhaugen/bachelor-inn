4fcc75143500dd33ecd748d6bae88ba9b0d3466a
AndreasBrandsnes <143383180+AndreasBrandsnes@users.noreply.github.com>
2026-09-07 14:38:51 +0200

    Removed prints from BT condition

--- scripts/AI/BehaviourTree/bt_conditions/cond_can_reach_attack_range.gd
+++ scripts/AI/BehaviourTree/bt_conditions/cond_can_reach_attack_range.gd
@@ -12,7 +12,6 @@ func tick(blackboard: BTBlackboard) -> BTNode.Status:
 	for cmd in moves:
 		if cmd is Move:
 			reachable.append(cmd.end_pos)
-	print("  reachable count: ", reachable.size())
 	
 	var best_origin: Vector3i = Vector3i.ZERO
 	var best_score: float = -99999.0
@@ -24,18 +23,11 @@ func tick(blackboard: BTBlackboard) -> BTNode.Status:
 		var min_dist_to_any_player: float = 99999.0
 
 		for other in state.units:
-			print("    RAW: ", other.data.unit_name if other.data else "null", 
-				" is_enemy=", other.state.is_enemy(),
-				" is_alive=", other.state.is_alive)
 			if other.state.is_enemy():
 				continue
 			if not other.state.is_alive:
 				continue
-			print("    checking other=", other.data.unit_name, 
-				" pos=", other.state.grid_position,
-				" from origin=", origin)
 			var dist: float = abs(other.state.grid_position.x - origin.x) + abs(other.state.grid_position.z - origin.z)
-			print("    DIST: ", other.data.unit_name, " dist=", dist)
 			if dist < min_dist_to_any_player:
 				min_dist_to_any_player = dist
 			if dist >= min_range and dist <= max_range and dist < closest_in_range_distance:
@@ -47,18 +39,13 @@ func tick(blackboard: BTBlackboard) -> BTNode.Status:
 		
 		var dist_to_ideal: float = abs(closest_in_range_distance - float(max_range))
 		var score: float = min_dist_to_any_player - dist_to_ideal
-		print("  origin=", origin, " target=", closest_in_range.data.unit_name, " attack_dist=", closest_in_range_distance, " min_player_dist=", min_dist_to_any_player, " score=", score)
 		if score > best_score:
 			best_score = score
 			best_origin = origin
 			best_target = closest_in_range
 	
 	if best_origin == Vector3i.ZERO or best_target == null:
-		print("  FAIL: no valid origin found")
 		return BTNode.Status.FAILURE
-	print("  SUCCESS: best_origin=", best_origin, 
-	" target=", best_target.data.unit_name,
-	" score=", best_score)
 	blackboard.target = best_target
 	blackboard.attack_origin = best_origin
 	return BTNode.Status.SUCCESS
